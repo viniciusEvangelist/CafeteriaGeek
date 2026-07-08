8f00c9674549926ec7ea1badbe00507cc40d50bd
Vinicius Evangelista Borges Pinto <viniciusy222@gmail.com>
2026-07-08 18:40:50 -0300

    Delete servico directory

--- servico/EventoGeekPromocao.java
+++
@@ -1,8 +0,0 @@
-package br.edu.cafeteria.servico;
-
-public class EventoGeekPromocao implements Promocional{
-    @Override
-    public double aplicarDesconto(double valor){
-        return valor * 0.9;
-    }
-}
--- servico/Promocional.java
+++
@@ -1,5 +0,0 @@
-package br.edu.cafeteria.servico;
-
-public interface Promocional {
-    double aplicarDesconto(double valor);
-}
