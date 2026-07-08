28740cea7a55308a6e3f6beb7f0b137a03562a5e
Vinicius Evangelista Borges Pinto <viniciusy222@gmail.com>
2026-07-08 18:40:34 -0300

    Delete excecao directory

--- excecao/EstoqueInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.cafeteria.excecao;
-
-public class EstoqueInsuficienteException extends Exception{
-    public EstoqueInsuficienteException(String mensagem){
-        super(mensagem);
-    }
-}
--- excecao/PontosInsuficientesException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.cafeteria.excecao;
-
-public class PontosInsuficientesException extends Exception{
-    public PontosInsuficientesException(String mensagem){
-        super(mensagem);
-    }
-}
